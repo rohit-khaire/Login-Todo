77475d87c5939e4d787da5b2ea9531eac6eede2a
Rohit Khaire <athlius31@gmail.com>
2025-11-02 13:21:55 +0530

    Change MongoDB connection database name

--- Login-Todo/backend/db/conn.js
+++ Login-Todo/backend/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/IPPractical')
+mongoose.connect('mongodb://localhost:27017/RohitTest')
 .then(() => console.log('Database connected'))
 .catch((err) => console.error('Database connection error:', err));
